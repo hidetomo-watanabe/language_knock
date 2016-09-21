98f9a6c3f9bc4fe6280a5b67fb44204fdf533c58
hidetomo <hidetomo.watanabe@soinn.com>
2016-09-21 02:06:08 +0000

    add 29

---
+++ 29.py
@@ -0,0 +1,18 @@
+import json
+import requests
+from subprocess import Popen, PIPE
+import my_wiki
+if __name__ == '__main__':
+    cmd = 'python -u 20.py'
+    p = Popen(cmd.strip().split(' '), stdout=PIPE, stderr=PIPE)
+    out, err = p.communicate()
+    england_text = out.decode('utf-8')
+    filename = my_wiki.generateBasicInfo(england_text)['国旗画像']
+    api_url = 'https://en.wikipedia.org/w/api.php' + \
+        '?format=json&action=query&prop=imageinfo&iiprop=url&titles=File:' + \
+        filename.replace(' ', '%20')
+    tmp_json = json.loads(requests.get(api_url).content.decode('utf-8'))
+    for tmp in tmp_json['query']['pages'].values():
+        if 'imageinfo' in tmp:
+            image_url = tmp['imageinfo'][0]['url']
+    print(image_url)
